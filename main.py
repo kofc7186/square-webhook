1f298f808b28d469727285c466f2de124f01afd8
Bob Callaway <bob.callaway@gmail.com>
2019-11-15 15:13:05 -0800

    pylint fixes

--- main.py
+++ main.py
@@ -1,44 +1,49 @@
+import base64
+import hmac
+import os
+
+from hashlib import sha1
 from flask import Response
 from werkzeug.exceptions import BadRequest, UnsupportedMediaType, MethodNotAllowed
-import base64
-import os
-import sys
-from hashlib import sha1
-import hmac
 
 from google.cloud import pubsub_v1
 
 # only configure stackdriver logging when running on GCP
-if os.environ.get('GCP_PROJECT',None):
+if os.environ.get('GCP_PROJECT', None):
     import google.cloud.logging
     from google.cloud.logging.resource import Resource
 
     log_client = google.cloud.logging.Client()
-    log_name = 'cloudfunctions.googleapis.com%2Fcloud-functions' 
+    log_name = 'cloudfunctions.googleapis.com%2Fcloud-functions'
 
     # Inside the resource, nest the required labels specific to the resource type
     res = Resource(type="cloud_function", labels={
-                        "function_name": os.getenv('FUNCTION_NAME'),
-                        "region": os.getenv('FUNCTION_REGION')
-                    })
+        "function_name": os.getenv('FUNCTION_NAME'),
+        "region": os.getenv('FUNCTION_REGION')
+        })
     logger = log_client.logger(log_name.format("GCP_PROJECT"))
 else:
     import logging
 
+
 def handle_webhook(request):
     """ Validates that the webhook came from Square and triggers the order creation process.
     This function needs to return with an HTTP 200 within 3 seconds or else the webhook call will
-    be retried. 
+    be retried.
     """
 
     if request.method != 'POST':
         raise MethodNotAllowed(valid_methods="POST")
 
     if 'Square-Initial-Delivery-Timestamp' in request.headers:
-        logging.debug("Delivery time of initial notification: {}".format(request.headers['Square-Initial-Delivery-Timestamp']))
+        logging.debug("Delivery time of initial notification: %s",
+                      request.headers['Square-Initial-Delivery-Timestamp'])
 
     if 'Square-Retry-Number' in request.headers:
-        logging.warn("Square has resent this notification {} times; reason given for the last failure is '{}'".format(request.headers['Square-Retry-Number'], request.headers['Square-Retry-Reason']))
+        logging.warning("Square has resent this notification %s times; "
+                        "reason given for the last failure is '%s'",
+                        request.headers['Square-Retry-Number'],
+                        request.headers['Square-Retry-Reason'])
 
     content_type = request.headers['content-type']
     if content_type == 'application/json':
@@ -46,48 +51,47 @@ def handle_webhook(request):
         # ensure the request is signed as coming from Square
         validate_square_signature(request)
 
-        if request_json and request_json.keys() >= {"merchant_id", "location_id", "event_type", "entity_id"}:
+        if request_json and request_json.keys() >= {"merchant_id",
+                                                    "location_id",
+                                                    "event_type",
+                                                    "entity_id"}:
             logging.debug(msg="notification content from webhook", data=request_json)
 
             # put message on topic to upsert order
-            #publisher = pubsub_v1.PublisherClient()
-            #topic_path = "projects/{}/topics/{}".format(os.environ["GCP_PROJECT_ID"], "orders")
-            #future = publisher.publish(topic_path, data=request_json.encode('utf-8'))
+            publisher = pubsub_v1.PublisherClient()
+            topic_path = "projects/{}/topics/{}".format(os.environ["GCP_PROJECT_ID"], "orders")
+            future = publisher.publish(topic_path, data=request_json.encode('utf-8'))
 
-            # this will block until the publish is complete; or raise an exception if the publish fails which
-            # should trigger Square to retry the notification
-            message_id = ''#future.result()
+            # this will block until the publish is complete;
+            # or raise an exception if the publish fails which should trigger Square to
+            # retry the notification
+            message_id = future.result()
             return Response(message_id, status=200)
-        else:
-            raise BadRequest(description="JSON is invalid, or missing required property")
-    else:
-        raise UnsupportedMediaType(description="Unknown content type: {}".format(content_type))
+
+        raise BadRequest(description="JSON is invalid, or missing required property")
+
+    raise UnsupportedMediaType(description="Unknown content type: {}".format(content_type))
+
 
 def validate_square_signature(request):
+    """ Validates the signature for the webhook notification provided within the request.
+    The HMAC-SHA1 digest is computed over the concatenation of the URL and the content body.
+
+    The X-Square-Signature HTTP request header specifies the signed digest provided by Square,
+    which should match what is calculated in this method.
+    """
     key = os.environ['SQUARE_WEBHOOK_SIGNATURE_KEY']
     string_to_sign = request.url.encode() + request.data
 
     # Generate the HMAC-SHA1 signature of the string, signed with your webhook signature key
-    string_signature = str(base64.b64encode(hmac.new(key.encode(), string_to_sign, sha1).digest()),'utf-8')
+    string_signature = str(base64.b64encode(hmac.new(key.encode(),
+                                                     string_to_sign,
+                                                     sha1).digest()), 'utf-8')
 
-    # Remove the trailing newline from the generated signature (this is a quirk of the Python library)
+    # Remove the trailing newline from the generated signature
     string_signature = string_signature.rstrip('\n')
 
     # Compare your generated signature with the signature included in the request
     if not hmac.compare_digest(string_signature, request.headers['X-Square-Signature']):
         raise ValueError("Square Signature could not be verified")
     return True
-
-def hello_error_2(request):
-    # [START functions_helloworld_error]
-    # WILL NOT be reported to Stackdriver Error Reporting, but will show up
-    # in logs
-    print(RuntimeError('I failed you (print to stdout)'))
-    logging.warn(RuntimeError('I failed you (logging.warn)'))
-    logging.error(RuntimeError('I failed you (logging.error)'))
-    sys.stderr.write('I failed you (sys.stderr.write)\n')
-
-    # This WILL be reported to Stackdriver Error Reporting
-    from flask import abort
-    return abort(500)
-    # [END functions_helloworld_error]
--- main_test.py
+++ main_test.py
@@ -1,25 +1,14 @@
-# Copyright 2019 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the 'License');
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an 'AS IS' BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# pylint: disable=redefined-outer-name,unused-argument
 
-import hmac
-from hashlib import sha1
 import base64
-
+import hmac
 import json
+from hashlib import sha1
+
 import flask
-from werkzeug.exceptions import BadRequest, UnsupportedMediaType, MethodNotAllowed
 import pytest
+
+from werkzeug.exceptions import BadRequest, UnsupportedMediaType, MethodNotAllowed
 
 import main
 
@@ -28,10 +17,11 @@ import main
 def app():
     return flask.Flask(__name__)
 
-key = "abc123def456"
+
+KEY = "abc123def456"
 @pytest.fixture
 def mock_set_env_webhook_signature_key(monkeypatch):
-    monkeypatch.setenv("SQUARE_WEBHOOK_SIGNATURE_KEY",key)
+    monkeypatch.setenv("SQUARE_WEBHOOK_SIGNATURE_KEY", KEY)
 
 
 def test_handle_webhook_empty_webhook_key(app, monkeypatch):
@@ -52,15 +42,19 @@ def test_handle_webhook_invalid_method(app, mock_set_env_webhook_signature_key):
 def test_handle_webhook_invalid_signature(app, mock_set_env_webhook_signature_key):
     base_url = "functions.googlecloud.com"
     path = "/test_handle_webhook_valid"
-    with app.test_request_context(method='POST', path=path, base_url=base_url, json={
-        "merchant_id":"merchant", 
-        "location_id":"location", 
-        "event_type":"event", 
-        "entity_id":"entity"},
-        headers = {"X-Square-Signature": "NOT_A_VALID_SIGNATURE"}):
+    with app.test_request_context(method='POST',
+                                  path=path,
+                                  base_url=base_url,
+                                  json={
+                                      "merchant_id": "merchant",
+                                      "location_id": "location",
+                                      "event_type": "event",
+                                      "entity_id": "entity"},
+                                  headers={"X-Square-Signature": "NOT_A_VALID_SIGNATURE"}):
         with pytest.raises(ValueError):
             main.handle_webhook(flask.request)
             pytest.fail("Square Signature could not be verified")
+
 
 def test_handle_webhook_empty_json(app):
     with app.test_request_context(method='POST', content_type='application/json'):
@@ -70,7 +64,7 @@ def test_handle_webhook_empty_json(app):
 
 
 def test_handle_webhook_send_non_json(app):
-    with app.test_request_context(method='POST', content_type='text/plain',data='abc123'):
+    with app.test_request_context(method='POST', content_type='text/plain', data='abc123'):
         with pytest.raises(UnsupportedMediaType):
             main.handle_webhook(flask.request)
             pytest.fail("JSON is invalid, or missing required property")
@@ -78,10 +72,10 @@ def test_handle_webhook_send_non_json(app):
 
 def test_handle_webhook_valid_json_no_signature(app, mock_set_env_webhook_signature_key):
     content = {
-        "merchant_id":"merchant", 
-        "location_id":"location", 
-        "event_type":"event", 
-        "entity_id":"entity"
+        "merchant_id": "merchant",
+        "location_id": "location",
+        "event_type": "event",
+        "entity_id": "entity"
     }
     with app.test_request_context(method='POST', json=content):
         with pytest.raises(KeyError):
@@ -93,16 +87,20 @@ def test_handle_webhook_valid(app, mock_set_env_webhook_signature_key):
     base_url = "functions.googlecloud.com"
     path = "/test_handle_webhook_valid"
     content = {
-        "merchant_id":"merchant", 
-        "location_id":"location", 
-        "event_type":"event", 
-        "entity_id":"entity"
+        "merchant_id": "merchant",
+        "location_id": "location",
+        "event_type": "event",
+        "entity_id": "entity"
     }
-    to_sign = "://" + base_url + path + json.dumps(content,sort_keys=True)
-    signature = base64.b64encode(hmac.new(key.encode(),to_sign.encode(),sha1).digest())
-    with app.test_request_context(method='POST', path=path, base_url=base_url, json=content, headers = {"X-Square-Signature": signature}):
+    to_sign = "://" + base_url + path + json.dumps(content, sort_keys=True)
+    signature = base64.b64encode(hmac.new(KEY.encode(), to_sign.encode(), sha1).digest())
+    with app.test_request_context(method='POST',
+                                  path=path,
+                                  base_url=base_url,
+                                  json=content,
+                                  headers={"X-Square-Signature": signature}):
         res = main.handle_webhook(flask.request)
         assert res.status == '200 OK'
 
-#TODO: mock up pubsub
-#TODO: test failure of pubsub call
+# TODO: mock up pubsub
+# TODO: test failure of pubsub call
