1e43ea92c0e25d32183905cf5e7d01ba2679e8ba
Bob Callaway <bobcallaway@users.noreply.github.com>
2020-01-11 17:36:10 -0500

    Test split (#9)
    * split tests
    
    * add pytest-mock, fix linting
    
    * fix topic name
    
    * add tests
    
    * fix flake errors
    
    * pylint fixes
    
    * pin requirements

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+""" This Cloud Function accepts the webhook invocation from Square, validates that it came from our
+    account, and places a message on the orders queue to start processing the order.
+
+    Returns a 200 response code if the message is succesfully published; if there is any error in
+    processing the message, a non-200 response code will be returned. This will trigger the retry
+    logic of the Square Webhook system.
+"""
 # pylint: disable=redefined-outer-name,unused-argument,no-member
 
 import base64
@@ -24,14 +31,38 @@ if os.environ.get('FUNCTION_REGION', None):  # pragma: no cover
     LOGGER.addHandler(HANDLER)
 
 
+def validate_message(request):
+    """ Validates message is well formed and has valid signature """
+    if request.method != 'POST':
+        raise MethodNotAllowed(valid_methods="POST")
+
+    content_type = request.headers['content-type']
+    if content_type != 'application/json':
+        raise UnsupportedMediaType(description="Unknown content type: {}".format(content_type))
+
+    request_json = request.get_json(silent=False)
+
+    # ensure the request is signed as coming from Square
+    try:
+        validate_square_signature(request)
+    except ValueError:
+        raise BadRequest(description="Signature could not be validated")
+
+    if not request_json or request_json.keys() < {"merchant_id",
+                                                  "location_id",
+                                                  "event_type",
+                                                  "entity_id"}:
+        raise BadRequest(description="JSON is invalid, or missing required property")
+
+    return request_json
+
+
 def handle_webhook(request):
     """ Validates that the webhook came from Square and triggers the order creation process.
     This function needs to return with an HTTP 200 within 3 seconds or else the webhook call will
     be retried.
     """
-
-    if request.method != 'POST':
-        raise MethodNotAllowed(valid_methods="POST")
+    request_json = validate_message(request)
 
     if 'Square-Initial-Delivery-Timestamp' in request.headers:
         LOGGER.debug("Delivery time of initial notification: %s",
@@ -43,39 +74,21 @@ def handle_webhook(request):
                      request.headers['Square-Retry-Number'],
                      request.headers['Square-Retry-Reason'])
 
-    content_type = request.headers['content-type']
-    if content_type == 'application/json':
-        request_json = request.get_json(silent=False)
+    # put message on topic to upsert order
+    publisher = pubsub_v1.PublisherClient()
+    topic_path = publisher.topic_path(os.environ["GCP_PROJECT"], "orders")
+    future = publisher.publish(topic_path, data=json.dumps(request_json).encode('utf-8'))
 
-        # ensure the request is signed as coming from Square
-        try:
-            validate_square_signature(request)
-        except ValueError:
-            raise BadRequest(description="Signature could not be validated")
-
-        if request_json and request_json.keys() >= {"merchant_id",
-                                                    "location_id",
-                                                    "event_type",
-                                                    "entity_id"}:
-            # put message on topic to upsert order
-            publisher = pubsub_v1.PublisherClient()
-            topic_path = publisher.topic_path(os.environ["GCP_PROJECT"], "orders")
-            future = publisher.publish(topic_path, data=json.dumps(request_json).encode('utf-8'))
-
-            # this will block until the publish is complete;
-            # or raise an exception if the publish fails which should trigger Square to
-            # retry the notification
-            try:
-                message_id = future.result(timeout=2)
-                return Response(message_id, status=200)
-            except pubsub_v1.publisher.exceptions.TimeoutError:
-                raise InternalServerError(description="Timeout publishing notification")
-            except Exception:
-                raise InternalServerError(description="Unknown error")
-
-        raise BadRequest(description="JSON is invalid, or missing required property")
-
-    raise UnsupportedMediaType(description="Unknown content type: {}".format(content_type))
+    # this will block until the publish is complete;
+    # or raise an exception if the publish fails which should trigger Square to
+    # retry the notification
+    try:
+        message_id = future.result(timeout=2)
+        return Response(message_id, status=200)
+    except pubsub_v1.publisher.exceptions.TimeoutError:
+        raise InternalServerError(description="Timeout publishing notification")
+    except Exception:
+        raise InternalServerError(description="Unknown error")
 
 
 def validate_square_signature(request):
