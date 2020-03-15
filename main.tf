3e1746442d2534fcb72c3045454ab8d9483d88b5
Bob Callaway <bob.callaway@gmail.com>
2020-03-15 18:25:19 -0400

    write into provider var

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 provider "google" {
+    credentials = var.GOOGLE_CREDENTIALS_CONTENT
 }
 
 resource "google_cloudfunctions_function" "handle_webhook" {
--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
 variable "SQUARE_WEBHOOK_SIGNATURE_KEY" {
     type = string
 }
+
+variable "GOOGLE_CREDENTIALS_CONTENT" {
+    type = string
+}
