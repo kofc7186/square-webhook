4876b8447e4cc23792f835ff6300b3b90e970508
Bob Callaway <bob.callaway@gmail.com>
2020-03-15 19:30:12 -0400

    upload to GCS bucket

--- main.tf
+++ main.tf
@@ -10,11 +10,39 @@ resource "google_cloudfunctions_function" "handle_webhook" {
 
     available_memory_mb = 128
     trigger_http = true
+    timeout = 3
     entry_point = "handle_webhook"
+
+    source_archive_bucket = "${google_storage_bucket.bucket.name}"
+    source_archive_object = "${google_storage_bucket_object.archive.name}"
 
     environment_variables = {
         SQUARE_WEBHOOK_SIGNATURE_KEY = var.SQUARE_WEBHOOK_SIGNATURE_KEY
     }
+}
+
+resource "google_storage_bucket" "bucket" {
+  name = "cloudfunction-deploy-test1"
+}
+
+data "archive_file" "http_trigger" {
+  type        = "zip"
+  output_path = "${path.module}/files/http_trigger.zip"
+  source {
+    content  = "${file("${path.module}/files/main.py")}"
+    filename = "main.py"
+  }
+  source {
+    content  = "${file("${path.module}/files/requirements.txt")}"
+    filename = "requirements.txt"
+  }
+}
+
+resource "google_storage_bucket_object" "archive" {
+  name   = "http_trigger.zip"
+  bucket = "${google_storage_bucket.bucket.name}"
+  source = "${path.module}/files/http_trigger.zip"
+  depends_on = ["data.archive_file.http_trigger"]
 }
 
 # IAM entry for all users to invoke the function
